9003d8f3b7fde312affd78a7e009386439d53028
steve-ongera <gadafisteve001@gmail.com>
2025-02-04 00:28:11 +0300

    modified views.py

--- myapp/views.py
+++ myapp/views.py
@@ -685,7 +685,7 @@ def student_update(request, pk):
         form = StudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
-            return redirect('student_list')
+            return redirect('students_list')
     else:
         form = StudentForm(instance=student)
     return render(request, 'students/student_form.html', {'form': form})
